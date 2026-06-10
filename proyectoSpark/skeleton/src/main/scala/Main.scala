3d4fea4e79f2817b7f37327ec44727c51531ca79
Joaquin <joaquin.cano@mi.unc.edu.ar>
2026-06-10 03:01:18 -0300

    feat: Ejercicio 4 (No completo)

--- proyectoSpark/skeleton/src/main/scala/Main.scala
+++ proyectoSpark/skeleton/src/main/scala/Main.scala
@@ -16,7 +16,12 @@ object Main {
 
     val sc = spark.sparkContext
 
-    val filePath = cmdArgs.subscriptionFile
+    // Definimos acumuladores para contar feeds y posts exitosos-descargados/fallidos-descartados
+    val successfulFeeds = sc.longAccumulator("successfulFeeds")
+    val failedFeeds = sc.longAccumulator("failedFeeds")
+    val totalPosts = sc.longAccumulator("totalPosts")
+    val totalPostsFailed = sc.longAccumulator("totalPostsFailed")
+
 
     val subscriptionOpts = if (!new java.io.File(filePath).exists()) {
       println(s"Error: Could not load $filePath - file not found")
@@ -57,21 +62,42 @@ object Main {
       val feedOpt = FileIO.downloadFeed(subscription.url)
       feedOpt match {
         case None =>
+          // Si falla la descarga, actualizamos el contador de feeds fallidos
+          failedFeeds.add(1)
           println(s"Warning: Failed to download from '${subscription.name}' (${subscription.url})")
           List.empty[Post]
         case Some(content) =>
+          // Si se descarga correctamente, actualizamos el contador de feeds exitosos
+          successfulFeeds.add(1)
           JsonParser.parsePosts(content, subscription.name)
       }
-    }
+    }.cache() // Cacheamos el RDD de posts para evitar recomputaciones, al ser llamados de nuevo para las estadísticas
 
+    // Aplicamos el filtro a los posts descargados y contamos los resultados usando los acumuladores
     val filteredPostsRDD = postsRDD.filter { post =>
-      post.title.nonEmpty && post.selftext.nonEmpty
-    }
 
-    val totalPosts    = postsRDD.count()
+      totalPosts.add(1) // Contamos cada post procesado
+      val valid = post.title.nonEmpty && post.selftext.nonEmpty
+      
+      if(!valid)
+        totalPostsFailed.add(1) // Contamos cada post que no pasa el filtro
+      
+      valid
+    }.cache() // Cacheamos el RDD para evitar recomputaciones, al ser llamados de nuevo para las estadísticas
+    
+    // Calculamos tiempos y estadísticas
+    val inicio = System.currentTimeMillis()
+    val totalPostsCount = postsRDD.count()
+    val fin = System.currentTimeMillis()
+    println(s"Tiempo para contar totalPosts: ${fin - inicio} ms")
+    
+    val inicio2 = System.currentTimeMillis()
     val filteredCount = filteredPostsRDD.count()
-    val droppedCount  = totalPosts - filteredCount
-
+    val fin2 = System.currentTimeMillis()
+    println(s"Tiempo para contar filteredPosts: ${fin2 - inicio2} ms")
+    
+    val droppedCount  = totalPostsCount - filteredCount
+   
     val avgChars: Long =
       if (filteredCount > 0)
         filteredPostsRDD
@@ -79,14 +105,15 @@ object Main {
           .sum() / filteredCount
       else 0L
 
-    val feedsSuccess = subscriptions.length
-    val feedsFailed  = 0
+    // Obtenemos los valores de los acumuladores para las estadísticas finales
+    val feedsSuccess = successfulFeeds.value
+    val feedsFailed  = failedFeeds.value
 
     val stats = Map(
       "feedsSuccess"  -> feedsSuccess,
       "feedsFailed"   -> feedsFailed,
-      "postsSuccess"  -> totalPosts.toInt,
-      "postsFailed"   -> 0,
+      "postsSuccess"  -> totalPosts.value.toInt,
+      "postsFailed"   -> totalPostsFailed.value.toInt,
       "postsFiltered" -> droppedCount.toInt,
       "avgChars"      -> avgChars.toInt
     )
